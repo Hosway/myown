b29f60fe99e329315b3972dde1b6d7cdc9c863c9
joshg1 <josuegarica626@hotmail.com>
2019-04-04 19:44:31 -0600

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import First from './Components/First'
+import Second from './Components/Second'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <img src={logo} className="App-logo" alt="logo" />
+
+       <First />
+
+       <div>
+         <h2>h2 heading, second div</h2>
+       </div>
+
+       <Second />
       </div>
     );
   }
---
+++ src/Components/Second.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Second() {
+  return <h2>second</h2>
+}
+
+export default Second
